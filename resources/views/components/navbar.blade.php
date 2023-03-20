88fca3ff8e5294c719a9d0e12d8fc26b64cfed16
Antonio Giannino <fragia30@gmail.com>
2023-03-20 17:43:05 +0100

    grafica aggiornata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg bg-transparent fixed-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Pizzeria</a>
+      <a class="navbar-brand text-white fs-3" href="#">Pizzeria</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link text-white fs-5" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link text-white fs-5" href="#">Link</a>
           </li>
           @guest
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-white fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Benvenuto, guest
             </a>
             <ul class="dropdown-menu">
@@ -36,7 +36,7 @@
         </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" aria-label="Search">
-          <button class="btn btn-outline-danger" type="submit">Cerca</button>
+          <button class="btn btn-outline-light" type="submit">Cerca</button>
         </form>
       </div>
     </div>
